9d55658255b82f9ea3942cf9c9559b7addfefdb3
coachhe <coachhe@git.code.oa.com>
2023-01-18 03:31:39 +0800

    : 2023-01-18 03:31:39

---
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AddServlet.java
@@ -0,0 +1,27 @@
+package com.coachhe.servlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String fname = request.getParameter("fname");
+        String priceStr = request.getParameter("price");
+        Integer price = Integer.parseInt(priceStr);
+        String fcountStr = request.getParameter("fcount");
+        Integer fcount = Integer.parseInt(fcountStr);
+        String remark = request.getParameter("remark");
+
+        System.out.println("fname=" + fname);
+        System.out.println("price=" + price);
+        System.out.println("fcount=" + fcount);
+        System.out.println("remark=" + remark);
+
+    }
+}
