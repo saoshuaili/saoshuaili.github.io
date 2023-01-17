07031cf804547212bd43bf60d9f815163ef2442c
coachhe <coachhe@git.code.oa.com>
2023-01-18 02:28:52 +0800

    : 2023-01-18 02:28:52

--- code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AddServlet.java
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AddServlet.java
@@ -13,6 +13,17 @@ import java.io.IOException;
 public class AddServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String fname = req.getParameter("fname");
+        String priceStr = req.getParameter("price");
+        int price = Integer.parseInt(priceStr);
+        String fcountStr = req.getParameter("fcount");
+        int fcount = Integer.parseInt(fcountStr);
+        String remark = req.getParameter("remark");
+
+        System.out.println("fname = " + fname);
+        System.out.println("price = " + price);
+        System.out.println("fount = " + fcount);
+        System.out.println("remark = " + remark);
+
     }
 }
