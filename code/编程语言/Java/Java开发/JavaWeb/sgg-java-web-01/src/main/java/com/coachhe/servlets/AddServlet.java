37eb7105e022c85491c71e94ece3c7574177f7e9
coachhe <coachhe@git.code.oa.com>
2023-01-18 02:18:52 +0800

    : 2023-01-18 02:18:52

--- code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AddServlet.java
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AddServlet.java
@@ -13,6 +13,6 @@ import java.io.IOException;
 public class AddServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        
     }
 }
