e62b8b5c5c452fd997fa176ace0c5b7b8f0d939d
coachhe <coachhe@git.code.oa.com>
2023-01-25 00:36:02 +0800

    : 2023-01-25 00:36:02

---
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-01/src/main/java/com/coachhe/servlets/AttrubuteServlet.java
@@ -0,0 +1,19 @@
+package com.coachhe.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: CoachHe
+ * @Date: 2023/1/25 00:28
+ * 演示向 Http Session 保存数据
+ */
+public class AttrubuteServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().setAttribute("uname", "coachhe");
+    }
+}
