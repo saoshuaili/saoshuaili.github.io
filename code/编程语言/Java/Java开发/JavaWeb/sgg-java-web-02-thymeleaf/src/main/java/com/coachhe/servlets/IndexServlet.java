ad5a94e13ce187c42a53da926dcd5673bb187cba
coachhe <coachhe@git.code.oa.com>
2023-01-25 16:51:57 +0800

    : 2023-01-25 16:51:57

--- code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/main/java/com/coachhe/servlets/IndexServlet.java
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/main/java/com/coachhe/servlets/IndexServlet.java
@@ -1,10 +1,31 @@
 package com.coachhe.servlets;
 
+import com.coachhe.fruit.dao.FruitDAO;
+import com.coachhe.fruit.dao.impl.FruitDAOImpl;
+import com.coachhe.fruit.pojo.Fruit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @Author: CoachHe
  * @Date: 2023/1/25 12:32
  * 用来展示 tmymeleaf
  */
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        List<Fruit> fruitList = fruitDAO.getFruitList();
+        // 保存至 session 作用域
+        HttpSession session = request.getSession();
+        session.setAttribute("fruitList", fruitList);
+    }
 }
