d6769921db79a1b3c03cd91f5741257575d0ff17
coachhe <coachhe@git.code.oa.com>
2023-01-25 23:42:56 +0800

    : 2023-01-25 23:42:56

--- code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/main/java/com/coachhe/servlets/IndexServlet.java
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/main/java/com/coachhe/servlets/IndexServlet.java
@@ -27,5 +27,11 @@ public class IndexServlet extends HttpServlet {
         List<Fruit> fruitList = fruitDAO.getFruitList();
         // 保存至 session 作用域
         HttpSession session = request.getSession();        session.setAttribute("fruitList", fruitList);
+        // 此处的视图名称是 index
+        // 那么 thymeleaf 会将这个逻辑视图名称对应到物理视图名称上去
+        // 逻辑视图名称： index
+        // 物理视图名称： view-prefix + 逻辑视图名称 + view-suffix
+        // 所以真实的视图名称是： /index.html
+        
     }
 }
