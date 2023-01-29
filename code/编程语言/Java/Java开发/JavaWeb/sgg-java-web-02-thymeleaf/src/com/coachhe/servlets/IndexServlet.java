4ed71c8240d39e0f44f0efcb333e0de2b281ba52
coachhe <coachhe>
2023-01-29 16:25:23 +0800

    : 2023-01-29 16:25:23

--- code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/com/coachhe/servlets/IndexServlet.java
+++ code/编程语言/Java/Java开发/JavaWeb/sgg-java-web-02-thymeleaf/src/com/coachhe/servlets/IndexServlet.java
@@ -25,12 +25,14 @@ public class IndexServlet extends ViewBaseServlet {
         FruitDAO fruitDAO = new FruitDAOImpl();
         List<Fruit> fruitList = fruitDAO.getFruitList();
         // 保存至 session 作用域
-        HttpSession session = request.getSession();        session.setAttribute("fruitList", fruitList);
+        HttpSession session = request.getSession();
+        session.setAttribute("fruitList", fruitList);
         // 此处的视图名称是 index
         // 那么 thymeleaf 会将这个逻辑视图名称对应到物理视图名称上去
         // 逻辑视图名称： index
         // 物理视图名称： view-prefix + 逻辑视图名称 + view-suffix
         // 所以真实的视图名称是： /index.html
+        // 也就是说，当用户访问 http://localhost:8080 时会自动访问 http://localhost:8080/index.html
         super.processTemplate("index", request, response);
     }
 }
