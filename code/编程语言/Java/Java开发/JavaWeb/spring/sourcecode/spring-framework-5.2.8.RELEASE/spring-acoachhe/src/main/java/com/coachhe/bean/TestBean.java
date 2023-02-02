1d1eb15e28d9f5f32ad9b7e7234ec8d8e654e277
coachhe <coachhe>
2023-02-02 13:21:06 +0800

    coachhe: 2023-02-02 13:21:05

---
+++ code/编程语言/Java/Java开发/JavaWeb/spring/sourcecode/spring-framework-5.2.8.RELEASE/spring-acoachhe/src/main/java/com/coachhe/bean/TestBean.java
@@ -0,0 +1,17 @@
+package com.coachhe.bean;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2023/2/2
+ * Time: 13:04
+ * Description:
+ */
+public class TestBean {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext;
+		System.out.println("Hello Spring");
+	}
+}
