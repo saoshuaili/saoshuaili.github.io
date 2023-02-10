507cadec5f98cf9159ac0f8dbc98cb41a8e6aad0
coachhe <coachhe>
2023-02-10 22:56:37 +0800

    : 2023-02-10 22:56:37

--- code/编程语言/Java/Java开发/sgg-java-web/sgg-java-web-06-servlet-api/src/com/coachhe/servlets/ApiServlet.java
+++ code/编程语言/Java/Java开发/sgg-java-web/sgg-java-web-06-servlet-api/src/com/coachhe/servlets/ApiServlet.java
@@ -1,7 +1,10 @@
 package com.coachhe.servlets;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 /**
@@ -11,13 +14,20 @@ import javax.servlet.http.HttpServlet;
  * Time: 13:23
  * Description:
  */
-
+@WebServlet(urlPatterns = "/api2", initParams = {
+        @WebInitParam(name = "name", value = "coachhe"),
+        @WebInitParam(name = "age", value = "27")
+})
 public class ApiServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         ServletConfig servletConfig = getServletConfig();
         String name = servletConfig.getInitParameter("name");
         System.out.println("init value = " + name);
+
+        ServletContext servletContext = getServletContext();
+        String contextConfigLocation = servletContext.getInitParameter("contextConfigLocation");
+        System.out.println("contextConfigLocation = " + contextConfigLocation);
     }
 }
 
