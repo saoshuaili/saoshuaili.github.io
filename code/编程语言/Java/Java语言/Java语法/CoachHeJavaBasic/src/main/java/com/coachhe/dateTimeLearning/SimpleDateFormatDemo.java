3742a5c434a2f96a802fcf5942060eeb04b943f1
coachhe <coachhe>
2023-02-27 17:42:46 +0800

    coachhe: 2023-02-27 17:42:46

--- code/编程语言/Java/Java语言/Java语法/CoachHeJavaBasic/src/main/java/com/coachhe/dateTimeLearning/SimpleDateFormatDemo.java
+++ code/编程语言/Java/Java语言/Java语法/CoachHeJavaBasic/src/main/java/com/coachhe/dateTimeLearning/SimpleDateFormatDemo.java
@@ -16,5 +16,22 @@ public class SimpleDateFormatDemo {
         Date now_date = new Date();
         SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
         System.out.println("当前时间为: " + ft.format(now_date));
+
+        //c的使用
+        System.out.printf("全部日期和时间信息：%tc%n",now_date);
+        //f的使用
+        System.out.printf("年-月-日格式：%tF%n",now_date);
+        //d的使用
+        System.out.printf("月/日/年格式：%tD%n",now_date);
+        //r的使用
+        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n",now_date);
+        //t的使用
+        System.out.printf("HH:MM:SS格式（24时制）：%tT%n",now_date);
+        //R的使用
+        System.out.printf("HH:MM格式（24时制）：%tR",now_date);
+
+        // 使用toString()显示日期和时间
+        System.out.printf("%1$s %2$tB %2$td, %2$tY",
+                "Due date:", now_date);
     }
 }
