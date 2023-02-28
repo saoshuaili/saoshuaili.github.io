149e4fcb69a55b85991dac22645b00ac5142eeaa
coachhe <coachhe>
2023-02-28 11:59:45 +0800

    coachhe: 2023-02-28 11:59:45

--- code/编程语言/Java/Java语言/Java语法/CoachHeJavaBasic/src/main/java/com/coachhe/dateTimeLearning/CalendarDemo.java
+++ code/编程语言/Java/Java语言/Java语法/CoachHeJavaBasic/src/main/java/com/coachhe/dateTimeLearning/CalendarDemo.java
@@ -13,6 +13,7 @@ import java.util.Calendar;
  */
 public class CalendarDemo {
 
+    // Calendar对象的创建实例
     @Test
     public void CalendarCreation() {
         // 创建一个代表系统当前日期的Calendar对象
@@ -21,6 +22,28 @@ public class CalendarDemo {
         // 创建一个指定日期的Calendar对象
         Calendar c1 = Calendar.getInstance();
         c1.set(2023, 2, 28);
+    }
+
+    // Calendar对象的使用
+    @Test
+    public void CalendarUsage(){
+        Calendar c = Calendar.getInstance();
+        c.set(2023, 2, 28);
+        // 可以只设置某个具体的值，例如把天置为10
+        c.set(Calendar.DATE, 10);
+        // 也可以把月份设置为3月
+        c.set(Calendar.MONTH, 3);
+
+        // 把对象的日期加上10，获取10天后的时间
+        c.add(Calendar.DATE, 10);
+        // 把对象的月份加上1，获取一个月后的时间
+        c.add(Calendar.MONTH, 1);
+        // 把对象的日期减去5，获取5天前的时间
+        c.add(Calendar.DATE, -5);
+
+        // 获取Calander对象的值信息
+        int year = c.get(Calendar.YEAR); // 获取年份
+        int month = c.get(Calendar.MONTH); // 
     }
 
 
