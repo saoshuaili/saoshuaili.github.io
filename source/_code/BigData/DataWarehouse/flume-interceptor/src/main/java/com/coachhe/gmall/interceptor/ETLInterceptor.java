49ebb7b558a7440b484f8c2d4359d369b63b3e7d
coachhe <coachhe>
2023-03-19 21:55:41 +0800

    vault backup: 2023-03-19 21:55:41

---
+++ source/_code/BigData/DataWarehouse/flume-interceptor/src/main/java/com/coachhe/gmall/interceptor/ETLInterceptor.java
@@ -0,0 +1,9 @@
+package com.coachhe.gmall.interceptor;/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2023/3/19
+ * Time: 21:54
+ * Description: 
+ */
+public class ETLInterceptor {
+}
