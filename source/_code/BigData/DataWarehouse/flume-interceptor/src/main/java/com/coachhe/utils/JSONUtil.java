368d52baba03329514299b5840145a307d76ebd0
coachhe <coachhe>
2023-03-19 22:09:42 +0800

    vault backup: 2023-03-19 22:09:41

--- source/_code/BigData/DataWarehouse/flume-interceptor/src/main/java/com/coachhe/utils/JSONUtil.java
+++ source/_code/BigData/DataWarehouse/flume-interceptor/src/main/java/com/coachhe/utils/JSONUtil.java
@@ -1,9 +1,23 @@
-package com.coachhe.utils;/**
+package com.coachhe.utils;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+
+/**
  * Created with IntelliJ IDEA.
  * User: coachhe
  * Date: 2023/3/19
  * Time: 21:58
- * Description: 
+ * Description:
  */
 public class JSONUtil {
+    public static boolean isJsonValidate(String log) {
+        try {
+            JSONObject.parseObject(log);
+            return true;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
