695ef9e4a11c04c8b77ad8092490134fa13b3c18
coachhe <624904389@qq.com>
2023-04-24 02:36:19 +0800

    vault backup: 2023-04-24 02:36:19

---
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/SteamAPI/StreamAPITest.java
@@ -0,0 +1,2 @@
+package com.coachhe.SteamAPI;public class StreamAPITest {
+}
--- source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/lambda表达式/lambdaTest.java
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/lambda表达式/lambdaTest.java
@@ -1,2 +1,25 @@
-package com.coachhe.lambda表达式;public class lambdaTest {
+package com.coachhe.lambda表达式;
+
+import java.util.Comparator;
+
+public class lambdaTest {
+    public static void main(String[] args) {
+
+        // 原始方法
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        };
+
+        // lambda表达式
+        Comparator<Integer> com2 = (o1, o2) -> {
+            return Integer.compare(o1, o2);
+        };
+
+        // 方法3
+        Comparator<Integer> com3 = Integer::compare;
+
+    }
 }
