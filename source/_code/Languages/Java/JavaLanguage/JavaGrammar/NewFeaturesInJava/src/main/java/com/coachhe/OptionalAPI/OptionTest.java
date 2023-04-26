69df35561a28c7f668e2fa3521348dbfb300e449
coachhe <624904389@qq.com>
2023-04-26 23:55:22 +0800

    vault backup: 2023-04-26 23:55:22

--- source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/OptionalAPI/OptionTest.java
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/OptionalAPI/OptionTest.java
@@ -1,11 +1,34 @@
 package com.coachhe.OptionalAPI;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.junit.Test;
+
+import java.util.Optional;
+
 public class OptionTest {
 
+    @Test
+    public void test1(){
+        Girl girl = new Girl();
+        girl = null;
+        Optional<Girl> optionalGirl = Optional.of(girl);
+    }
 
 
 }
 
 @Data
+@ToString
 class Girl{
+}
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+class Boy {
+    private Girl girl;
 }
