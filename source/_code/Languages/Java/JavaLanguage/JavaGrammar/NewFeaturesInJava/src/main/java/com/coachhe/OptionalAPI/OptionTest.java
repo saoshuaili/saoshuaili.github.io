b87b626dd25de0a02c5395e3c49de5f3ba40590d
coachhe <624904389@qq.com>
2023-04-26 23:56:28 +0800

    vault backup: 2023-04-26 23:56:28

--- source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/OptionalAPI/OptionTest.java
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/OptionalAPI/OptionTest.java
@@ -10,6 +10,11 @@ import java.util.Optional;
 
 public class OptionTest {
 
+    /**
+     * Optional.of(T t) 创建一个Optional实例，t必须非空
+     * Optional.empty() 创建一个空的Optional实例
+     * Optional.ofNullable(T t) t可以为null
+     */
     @Test
     public void test1(){
         Girl girl = new Girl();
