4beee627036921997e8df9854a8387edf85a6685
coachhe(coachhe) <coachhe@tencent.com>
2023-04-28 13:31:24 +0800

    vault backup: 2023-04-28 13:31:24

---
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/Main.java
@@ -0,0 +1,8 @@
+package com.coachhe;
+
+// Future接口可以为主线程开一个分支任务，专门为主线程处理耗时和费力的复杂业务
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
