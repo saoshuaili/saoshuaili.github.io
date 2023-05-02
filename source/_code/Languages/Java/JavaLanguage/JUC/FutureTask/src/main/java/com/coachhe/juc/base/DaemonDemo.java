2fa016ed81b70ace3fa5b391eb8c3fdf2239e3b2
coachhe(coachhe) <coachhe@tencent.com>
2023-05-02 19:10:57 +0800

    vault backup: 2023-05-02 19:10:57

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/juc/base/DaemonDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/juc/base/DaemonDemo.java
@@ -1,12 +1,26 @@
 package com.coachhe.juc.base;
 
+import java.util.concurrent.TimeUnit;
+
 public class DaemonDemo {
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + "\t 开始运行， " +
                     (Thread.currentThread().isDaemon() ? "守护线程" : "用户线程"));
+            while (true) {
+
+            }
         }, "t1");
-        
+
         t1.start();
+
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t ---end 主线程");
     }
 }
