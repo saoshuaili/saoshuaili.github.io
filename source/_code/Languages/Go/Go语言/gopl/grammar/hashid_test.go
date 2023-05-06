ed1a681e069efe458d8384b0bac941200c74a727
coachhe(coachhe) <coachhe@tencent.com>
2023-05-06 10:39:23 +0800

    vault backup: 2023-05-06 10:39:23

--- source/_code/Languages/Go/Go语言/gopl/grammar/hashid_test.go
+++ source/_code/Languages/Go/Go语言/gopl/grammar/hashid_test.go
@@ -1,9 +1,61 @@
 package grammar
 
 import (
+	"fmt"
+	"github.com/sony/sonyflake"
+	"github.com/speps/go-hashids"
 	"testing"
+	"unicode/utf8"
 )
 
-func TestHashIds(t *testing.T) {
+// Defiens alphabet.
+const (
+	Alphabet62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	Alphabet36 = "abcdefghijklmnopqrstuvwxyz1234567890"
+)
 
+var sf *sonyflake.Sonyflake
+
+func init() {
+	var st sonyflake.Settings
+	st.MachineID = func() (uint16, error) {
+		ip := "127.0.0.1"
+
+		return uint16([]byte(ip)[2])<<8 + uint16([]byte(ip)[3]), nil
+	}
+
+	sf = sonyflake.NewSonyflake(st)
+}
+
+func Reverse(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
+
+func TestHashIds(t *testing.T) {
+	hd := hashids.NewData()
+	hd.Alphabet = Alphabet36
+	hd.MinLength = 6
+	hd.Salt = "x20k5x"
+
+	h, err := hashids.NewWithData(hd)
+	if err != nil {
+		panic(err)
+	}
+
+	uid := 5
+	prefix := "cronjob-"
+
+	i, err := h.Encode([]int{int(uid)})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(prefix + Reverse(i))
 }
