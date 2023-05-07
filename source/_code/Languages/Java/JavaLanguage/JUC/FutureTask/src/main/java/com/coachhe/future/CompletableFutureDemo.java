b6d804dd5d8bfadea3c9e8c01d866d9754f04569
coachhe <624904389@qq.com>
2023-05-07 22:56:29 +0800

    vault backup: 2023-05-07 22:56:29

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/CompletableFutureDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/CompletableFutureDemo.java
@@ -1,4 +1,27 @@
 package com.coachhe.future;
 
+import java.util.concurrent.Callable;
+
 public class CompletableFutureDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class MyThread2 implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
 }
