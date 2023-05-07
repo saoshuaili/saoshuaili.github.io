24bfc440239b95b93d7f238c2124329b55f5877f
coachhe <624904389@qq.com>
2023-05-08 00:16:32 +0800

    vault backup: 2023-05-08 00:16:32

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/FutureTaskDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/FutureTaskDemo.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
-public class CompletableFutureDemo {
+public class FutureTaskDemo {
 
     public static void main(String[] args) {
         FutureTask<String> futureTask = new FutureTask<>(new MyThread2());
