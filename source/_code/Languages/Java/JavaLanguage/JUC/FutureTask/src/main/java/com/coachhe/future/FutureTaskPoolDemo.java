91c9a1dbe5b48b887823389afdf5b99cfd2e9df5
coachhe <624904389@qq.com>
2023-05-08 00:57:32 +0800

    vault backup: 2023-05-08 00:57:32

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/FutureTaskPoolDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/FutureTaskPoolDemo.java
@@ -1,5 +1,6 @@
 package com.coachhe.future;
 
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,8 +12,22 @@ import java.util.concurrent.TimeUnit;
 public class FutureTaskPoolDemo {
 
     public static void main(String[] args) {
-        // 三个任务，目前只有一个main线程来处理，要耗时多久
 
+        FutureTask<String> futureTask1 = new FutureTask<>(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "task1 over";
+        });
+        Thread t1 = new Thread(futureTask1, "t1");
+        t1.start();
+
+    }
+
+    private static void m1() {
+        // 三个任务，目前只有一个main线程来处理，要耗时多久
         long startTime = System.currentTimeMillis();
 
         // 暂停毫秒
@@ -26,8 +41,5 @@ public class FutureTaskPoolDemo {
 
         long endTime = System.currentTimeMillis();
         System.out.println("---cost time: " + (endTime - startTime) + " 毫秒");
-
-
-
     }
 }
