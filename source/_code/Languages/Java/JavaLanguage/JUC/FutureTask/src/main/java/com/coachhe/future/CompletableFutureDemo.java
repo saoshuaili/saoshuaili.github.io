b62a00a46d21020e4422f30ee3e000b7437287c6
coachhe(coachhe) <coachhe@tencent.com>
2023-05-09 10:14:16 +0800

    vault backup: 2023-05-09 10:14:15

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/CompletableFutureDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/CompletableFutureDemo.java
@@ -1,8 +1,24 @@
 package com.coachhe.future;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: CoachHe
  * @Date: 2023/5/9 10:06
  */
 public class CompletableFutureDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            // 停顿几秒钟
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(voidCompletableFuture.get());
+    }
 }
