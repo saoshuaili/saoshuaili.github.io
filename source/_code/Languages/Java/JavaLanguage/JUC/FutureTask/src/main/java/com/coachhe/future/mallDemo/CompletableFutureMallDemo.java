3ba1e59944666450858d3525b08b7d3f5fc27a9d
coachhe <624904389@qq.com>
2023-05-11 00:07:36 +0800

    vault backup: 2023-05-11 00:07:35

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
@@ -13,7 +18,20 @@ import lombok.experimental.Accessors;
  * @DATE: 2023/5/10 23:06
  */
 public class CompletableFutureMallDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            return "hello 1234";
+        }, executorService);
+
+        System.out.println(stringCompletableFuture.get());
+        
+        executorService.shutdown();
+    }
+
+    // 链式语法使用
+    private static void chainReactDemo() {
         Student student = new Student();
         student.setId(12).setStudentName("li4").setMajor("english");
     }
