36cbdb1b6d2b293528b536d775d4ca90893a5383
coachhe <624904389@qq.com>
2023-05-10 23:09:36 +0800

    vault backup: 2023-05-10 23:09:35

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
@@ -1,5 +1,11 @@
 package com.coachhe.future.mallDemo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
 /**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
@@ -8,11 +14,16 @@ package com.coachhe.future.mallDemo;
  */
 public class CompletableFutureMallDemo {
     public static void main(String[] args) {
-
+        Student student = new Student();
+        student.setId(12).setStudentName("li4").setMajor("english");
     }
 }
 
 @Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 class Student {
     private Integer id;
     private String studentName;
