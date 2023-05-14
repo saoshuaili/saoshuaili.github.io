d9ee69ed4ff74fd28033cfaa860a2b60e85af093
coachhe <624904389@qq.com>
2023-05-15 01:00:10 +0800

    vault backup: 2023-05-15 01:00:10

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/mallDemo/CompletableFutureMallDemo.java
@@ -16,17 +16,16 @@ import java.util.stream.Collectors;
  * @PROJECT_NAME: JUC
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/11 0:35
- * @DESCRIPTION:
- * 1需求说明
+ * @DESCRIPTION: 1需求说明
  * 1.1同一款产品，同时搜索出同款产品在各大电商平台的售价；
  * 1.2同一款产品，同时搜索出本产品在同一个电商平台下，各个入驻卖家售价是多少
- *
+ * <p>
  * 2输出返回：
  * 出来结果希望是同款产品的在不同地方的价格清单列表， 返回一个List<String>
  * 《mysql》in jd price is 88.05
  * 《mysql》in dang dang price is 86.11
  * 《mysql》in tao bao price is 90.43
- *
+ * <p>
  * 3解决方案，比对同一个商品在各个平台上的价格，要求获得一个清单列表
  * 1   stepbystep   ， 按部就班， 查完京东查淘宝， 查完淘宝查天猫......
  * 2   all in       ，万箭齐发，一口气多线程异步任务同时查询。。。
@@ -41,11 +40,12 @@ public class CompletableFutureMallDemo {
 
     /**
      * step by step
+     *
      * @param list
      * @param productName
      * @return
      */
-    public static List<String> getPriceStepByStep(List<NetMall> list, String productName){
+    public static List<String> getPriceStepByStep(List<NetMall> list, String productName) {
         return list
                 .stream()
                 .map(netMall ->
@@ -57,18 +57,24 @@ public class CompletableFutureMallDemo {
 
     /**
      * 万箭齐发
+     *
      * @param list
      * @param productName
      * @return
      */
     public static List<String> getPriceByCompletableFuture(List<NetMall> list, String productName) {
-        return list.stream().map(netMall -> CompletableFuture.supplyAsync(() -> String.format(productName + " in %s pr=ice is %.2f",
-                netMall.getNetMallName(),
-                netMall.calcPrice(productName))))
+        return list.stream()
+                .map(netMall -> {
+                            System.out.printf("map1 print: %s\n", netMall.getNetMallName());
+                            return CompletableFuture.supplyAsync(
+                                    () -> String.format(productName + " in %s pr=ice is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName))
+                            );
+                        }
+                )
                 .toList()
                 .stream()
                 .map(s -> {
-                    System.out.printf("map print: %s\n", s.join());
+                    System.out.printf("map2 print: %s\n", s.join());
                     return s.join();
                 })
                 .collect(Collectors.toList());
