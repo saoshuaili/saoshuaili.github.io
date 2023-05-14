ce6353be257a1e16f7303e2f606aaba3cc7f09f2
coachhe <624904389@qq.com>
2023-05-15 01:30:27 +0800

    vault backup: 2023-05-15 01:30:27

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/completableFutureDemo/CompletableFutureAPI.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/completableFutureDemo/CompletableFutureAPI.java
@@ -13,6 +13,42 @@ import java.util.concurrent.TimeUnit;
 public class CompletableFutureAPI {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        completeAPIDemo();
+        CompletableFuture.supplyAsync(() -> {
+            // 暂停几秒钟线程
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("111");
+            return 1024;
+        }).thenApply(f -> {
+            System.out.println("222");
+            return f + 1;
+        }).thenApply(f -> {
+            System.out.println("333");
+            return f + 1;
+        }).whenCompleteAsync((v,e) -> {
+            System.out.println("---v: " + v);
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            return null;
+        });
+
+        System.out.println("------ 主线程结束");
+
+        // 主线程不立即结束，否则默认使用的线程池会关闭
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void completeAPIDemo() throws InterruptedException, ExecutionException {
         CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
             try {
                 TimeUnit.SECONDS.sleep(2); // 执行需要2秒
