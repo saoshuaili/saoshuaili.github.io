c375b599066b7cdd3d8848035062de14d3de54fd
coachhe <624904389@qq.com>
2023-05-14 11:57:33 +0800

    vault backup: 2023-05-14 11:57:33

--- source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/SteamAPI/SteamAPIOrder.java
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/SteamAPI/SteamAPIOrder.java
@@ -1,5 +1,9 @@
 package com.coachhe.SteamAPI;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.stream.Stream;
+
 /**
  * @PROJECT_NAME: NewFeaturesInJava
  * @DESCRIPTION: 测试Steam中函数的执行顺序
@@ -7,5 +11,12 @@ package com.coachhe.SteamAPI;
  * @DATE: 2023/5/14 11:53
  */
 public class SteamAPIOrder {
-
+    public static void main(String[] args) {
+        Stream.of("a", "b", "c", "d", "e").map(
+                a -> {
+                    System.out.println(a);
+                    return a.toUpperCase();
+                }
+        );
+    }
 }
