4a3f7b957c0e52bb7e682aa5ca2c71456137c48d
coachhe <624904389@qq.com>
2023-05-15 01:21:12 +0800

    vault backup: 2023-05-15 01:21:12

---
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/future/completableFutureDemo/CompletableFutureAPI.java
@@ -0,0 +1,8 @@
+package com.coachhe.future.completableFutureDemo;/** 
+ * @PROJECT_NAME: JUC
+ * @DESCRIPTION:
+ * @AUTHOR: CoachHe
+ * @DATE: 2023/5/15 1:21
+ */
+public class CompletableFutureAPI {
+}
