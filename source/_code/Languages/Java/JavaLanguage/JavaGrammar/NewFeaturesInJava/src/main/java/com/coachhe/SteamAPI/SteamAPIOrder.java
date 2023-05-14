74727dafd248dca0d904d74e0005ff0865d1d4cc
coachhe <624904389@qq.com>
2023-05-15 00:01:16 +0800

    vault backup: 2023-05-15 00:01:16

--- source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/SteamAPI/SteamAPIOrder.java
+++ source/_code/Languages/Java/JavaLanguage/JavaGrammar/NewFeaturesInJava/src/main/java/com/coachhe/SteamAPI/SteamAPIOrder.java
@@ -2,6 +2,8 @@ package com.coachhe.SteamAPI;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -14,7 +16,43 @@ import java.util.stream.Stream;
 public class SteamAPIOrder {
     public static void main(String[] args) {
 //        streamOrder();
-        Stream.of("a", "b", "c", "d", "e");
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.parallelStream()
+                .map(a -> {
+                            try {
+                                System.out.printf("map %s start\n", a);
+                                TimeUnit.SECONDS.sleep(2);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.printf("map %s end\n", a);
+                            return a.toUpperCase();
+                        }
+                )
+                .filter(a -> {
+                    try {
+                        System.out.printf("filter %s start\n", a);
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.printf("filter %s end\n", a);
+                    return a.equals("B");
+                })
+                .forEach(a -> {
+                    try {
+                        System.out.printf("foreach %s start\n", a);
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.printf("foreach %s end\n", a);
+                });
+
 
     }
 
