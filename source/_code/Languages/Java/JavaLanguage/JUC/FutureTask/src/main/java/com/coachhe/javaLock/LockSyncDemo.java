c70854986cf67d412588861a093b62fd47f8142c
coachhe <624904389@qq.com>
2023-05-16 01:32:30 +0800

    vault backup: 2023-05-16 01:32:30

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/LockSyncDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/LockSyncDemo.java
@@ -1,8 +1,21 @@
-package com.coachhe.javaLock;/** 
+package com.coachhe.javaLock;
+
+/**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/16 1:30
  */
 public class LockSyncDemo {
+    Object object = new Object();
+
+    public void m1() {
+        synchronized (object) {
+            System.out.println("---- hello synchronized code block");
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
