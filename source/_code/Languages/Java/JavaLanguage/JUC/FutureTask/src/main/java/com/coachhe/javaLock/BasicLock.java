c41b3411f2e635e2edd2a20dd06b033ebd3164ef
coachhe(coachhe) <coachhe@tencent.com>
2023-05-15 14:13:17 +0800

    vault backup: 2023-05-15 14:13:17

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/BasicLock.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/BasicLock.java
@@ -1,7 +1,48 @@
-package com.coachhe.javaLock;/** 
+package com.coachhe.javaLock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
  * @USER: coachhe
  * @DATE: 2023/5/15 14:06
- */public class BasicLock {
+ */
+public class BasicLock {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        Phone phone2 = new Phone();
+
+        new Thread(phone::sendEmail, "a").start();
+
+        // 暂停200毫秒，保证a线程先启动
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(phone2::sendSMS, "b").start();
+
+    }
+}
+
+class Phone {
+    public synchronized void sendEmail(){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("------sendEmail");
+    }
+
+    public synchronized void sendSMS() {
+        System.out.println("------sendSMS");
+    }
+
+    public void hello() {
+        System.out.println("-------hello");
+    }
+
 }
