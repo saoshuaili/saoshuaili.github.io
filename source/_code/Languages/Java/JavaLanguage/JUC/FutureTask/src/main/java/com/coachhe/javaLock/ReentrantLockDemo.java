15d6deae33f4acf5cd32b5739d65aca439a7a8c5
coachhe <624904389@qq.com>
2023-05-18 00:52:33 +0800

    vault backup: 2023-05-18 00:52:32

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/ReentrantLockDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/ReentrantLockDemo.java
@@ -1,8 +1,29 @@
-package com.coachhe.javaLock;/** 
+package com.coachhe.javaLock;
+
+/**
  * @PROJECT_NAME: JUC
- * @DESCRIPTION:
+ * @DESCRIPTION: 可重入锁，不是 ReEntrantLock类
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/18 0:49
  */
 public class ReentrantLockDemo {
+
+    // 在同步块中实现可重入
+    public static void m1(){
+        final Object objectLockA = new Object();
+
+        new Thread(() -> {
+            synchronized (objectLockA) {
+                System.out.println("-- 外层调用 --");
+                synchronized (objectLockA) {
+                    System.out.println("-- 内层调用 --");
+                }
+            }
+        }, "a").start();
+    }
+
+    public static void main(String[] args) {
+        m1();
+    }
+
 }
