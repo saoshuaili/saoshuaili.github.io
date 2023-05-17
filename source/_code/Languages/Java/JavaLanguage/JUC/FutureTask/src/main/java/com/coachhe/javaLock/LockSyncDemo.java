d0230d3eb7a319cb5b4de623551487c85636b313
coachhe <624904389@qq.com>
2023-05-18 00:15:07 +0800

    vault backup: 2023-05-18 00:15:06

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/LockSyncDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/LockSyncDemo.java
@@ -1,5 +1,7 @@
 package com.coachhe.javaLock;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
@@ -9,18 +11,27 @@ package com.coachhe.javaLock;
 public class LockSyncDemo {
     final Object object = new Object();
 
+    // 悲观锁 demo1
     public void m1() {
         synchronized (object) {
             System.out.println("---- hello synchronized code block");
         }
     }
 
+    // 悲观锁 demo2
     public synchronized void m2() {
         System.out.println("---- hello synchronized code m2");
     }
 
+    // 悲观锁 demo3
     public synchronized static void m3() {
         System.out.println("---- hello m3");
+    }
+
+    // 乐观锁 demo
+    public void m4() {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.incrementAndGet();
     }
 
     public static void main(String[] args) {
