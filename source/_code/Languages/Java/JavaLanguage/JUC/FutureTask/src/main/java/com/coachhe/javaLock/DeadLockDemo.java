cc58f1f0af081bf2fda1a78a1b629e80723fcf3b
coachhe <624904389@qq.com>
2023-05-18 01:10:39 +0800

    vault backup: 2023-05-18 01:10:38

---
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/DeadLockDemo.java
@@ -0,0 +1,20 @@
+package com.coachhe.javaLock;
+
+/**
+ * @PROJECT_NAME: JUC
+ * @DESCRIPTION: 死锁案例
+ * @AUTHOR: CoachHe
+ * @DATE: 2023/5/18 1:09
+ */
+public class DeadLockDemo {
+    public void m1(){
+        Object o1 = new Object();
+        Object o2 = new Object();
+
+        new Thread(() -> {
+            synchronized (o1) {
+                System.out.println(Thread.currentThread().getName() + "\t 持有o1");
+            }
+        })
+    }
+}
--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/ReentrantLockDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/javaLock/ReentrantLockDemo.java
@@ -58,6 +58,6 @@ public class ReentrantLockDemo {
         }, "t1").start();
     }
 
-    
+
 
 }
