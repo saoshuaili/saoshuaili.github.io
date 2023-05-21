ef6dd4624eff9f2fad31c893498909a1afa34981
coachhe <624904389@qq.com>
2023-05-21 23:49:33 +0800

    vault backup: 2023-05-21 23:49:32

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/interrupt/InterruptDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/interrupt/InterruptDemo.java
@@ -1,8 +1,34 @@
-package com.coachhe.interrupt;/** 
+package com.coachhe.interrupt;
+
+/**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/21 22:48
  */
 public class InterruptDemo {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println(Thread.currentThread().getName() + "\t stop");
+                    break;
+                }
+            }
+        }, "t1");
+        t1.start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+
 }
