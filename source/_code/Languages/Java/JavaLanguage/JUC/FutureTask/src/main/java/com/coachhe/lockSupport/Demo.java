c2ed8f586936baa24b5f6d4f38963eab658e5b73
coachhe <624904389@qq.com>
2023-05-24 00:40:40 +0800

    vault backup: 2023-05-24 00:40:40

---
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/lockSupport/Demo.java
@@ -0,0 +1,13 @@
+package com.coachhe.lockSupport;
+
+/**
+ * @PROJECT_NAME: JUC
+ * @DESCRIPTION:
+ * @AUTHOR: CoachHe
+ * @DATE: 2023/5/24 0:39
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
