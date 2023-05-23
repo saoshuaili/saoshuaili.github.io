7f8425c5d48ed76bc87374167c4d04a8dae1a0ca
coachhe <624904389@qq.com>
2023-05-24 01:01:39 +0800

    vault backup: 2023-05-24 01:01:39

--- source/_code/Languages/python/basic/1.py
+++ source/_code/Languages/python/basic/1.py
@@ -14,18 +14,23 @@ cursor = db.cursor()
 
 # 使用 execute()  方法执行 SQL 查询
 cursor.execute("SHOW databases")
+# cursor.execute("select * from cloudLearn.hello")
 
 # 使用 fetchone() 方法获取单条数据.
 data = cursor.fetchall()
 
 print(data)
+# (('information_schema',), ('cloudLearn',), ('cronjob',), ('gmall',), ('hive',), ('maxwell',), ('metastore',), ('mysql',), ('performance_schema',), ('rc',), ('sys',), ('work_order',))
 
 for dbname in data:
+    #('work_order',)
     dbname_re = str(dbname).replace("(", "").replace("'", "").replace(")","").replace(",","")
+    #print("current database is : %s" % dbname_re)
     cursor.execute("use %s" % dbname_re)
     cursor.execute("show tables")
     data2 = cursor.fetchall()
     for tmp in data2:
+        #('casbin_rule',)
         print("|%s|%s|" % (dbname_re, str(tmp).replace("(", "").replace("'", "").replace(")","").replace(",","")))
 
 # 关闭数据库连接
