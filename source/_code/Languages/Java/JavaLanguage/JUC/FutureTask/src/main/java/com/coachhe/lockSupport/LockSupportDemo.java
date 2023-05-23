ec90bd563618259974385d5f23591ab71f3ec6a8
coachhe <624904389@qq.com>
2023-05-24 01:28:47 +0800

    vault backup: 2023-05-24 01:28:47

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/lockSupport/LockSupportDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/lockSupport/LockSupportDemo.java
@@ -1,6 +1,9 @@
 package com.coachhe.lockSupport;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @PROJECT_NAME: JUC
@@ -11,6 +14,46 @@ import java.util.concurrent.TimeUnit;
 public class LockSupportDemo {
 
     public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+
+        Thread t1 = new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t---come in");
+                condition.await();
+                System.out.println(Thread.currentThread().getName() + "\t--- 被唤醒");
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "t1");
+        t1.start();
+
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Thread t2 = new Thread(() -> {
+            lock.lock();
+            try {
+                condition.signal();
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t 我要进行唤醒");
+        }, "t2");
+        t2.start();
+    }
+
+    // 展示先notify后wait的报错通知
+    private static void waitAndNotifyDemo03() {
         Object objectLock = new Object();
 
         Thread t1 = new Thread(() -> {
@@ -41,7 +84,7 @@ public class LockSupportDemo {
     }
 
     // 展示了去掉synchronized之后对锁的操作会报错
-    private static void () {
+    private static void waitAndNotifyDemo01() {
         Object objectLock = new Object();
 
         Thread t1 = new Thread(() -> {
@@ -71,7 +114,7 @@ public class LockSupportDemo {
     }
 
     // 表明正常情况下，wait和notify的用法
-    private static void LockSupportDemo01() {
+    private static void waitAndNotifyDemo02() {
         Object objectLock = new Object();
 
         Thread t1 = new Thread(() -> {
