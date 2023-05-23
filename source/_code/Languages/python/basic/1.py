13cfcc18a4101084729a81d4a83de8d9bef1e566
coachhe <624904389@qq.com>
2023-05-24 00:47:34 +0800

    vault backup: 2023-05-24 00:47:34

--- source/_code/Languages/python/basic/1.py
+++ source/_code/Languages/python/basic/1.py
@@ -19,17 +19,13 @@ cursor.execute("SHOW databases")
 data = cursor.fetchall()
 
 print(data)
-# (('information_schema',), ('cloudLearn',), ('cronjob',), ('gmall',), ('hive',), ('maxwell',), ('metastore',), ('mysql',), ('performance_schema',), ('rc',), ('sys',), ('work_order',))
 
 for dbname in data:
-    #('work_order',)
     dbname_re = str(dbname).replace("(", "").replace("'", "").replace(")","").replace(",","")
-    #print("current database is : %s" % dbname_re)
     cursor.execute("use %s" % dbname_re)
     cursor.execute("show tables")
     data2 = cursor.fetchall()
     for tmp in data2:
-        #('casbin_rule',)
         print("|%s|%s|" % (dbname_re, str(tmp).replace("(", "").replace("'", "").replace(")","").replace(",","")))
 
 # 关闭数据库连接
