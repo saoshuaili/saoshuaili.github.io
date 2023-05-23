b4556c83a85e5da4fdebedaa2a0c0a9b6557fe09
coachhe <624904389@qq.com>
2023-05-24 01:16:43 +0800

    vault backup: 2023-05-24 01:16:43

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/lockSupport/LockSupportDemo.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/lockSupport/LockSupportDemo.java
@@ -15,6 +15,37 @@ public class LockSupportDemo {
         Object objectLock = new Object();
 
         Thread t1 = new Thread(() -> {
+//            synchronized (objectLock) {
+                System.out.println(Thread.currentThread().getName() + "\t ---come in");
+                try {
+                    objectLock.wait();  /// --- 先等待
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//            }
+        }, "t1");
+        t1.start();
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread t2 = new Thread(() -> {
+//            synchronized (objectLock) {
+                objectLock.notify(); // ---唤醒
+                System.out.println(Thread.currentThread().getName() + "\t --- 发出通知");
+//            }
+        }, "t2");
+        t2.start();
+
+    }
+
+    // 表明正常情况下，wait和notify的用法
+    private static void LockSupportDemo01() {
+        Object objectLock = new Object();
+
+        Thread t1 = new Thread(() -> {
             synchronized (objectLock) {
                 System.out.println(Thread.currentThread().getName() + "\t ---come in");
                 try {
