80d0245b2bfb49603bb22393f04c476143c82822
coachhe <624904389@qq.com>
2023-05-26 01:13:40 +0800

    vault backup: 2023-05-26 01:13:40

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/VolatileVisibilityTest.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/VolatileVisibilityTest.java
@@ -1,5 +1,7 @@
 package com.coachhe.jmm;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -9,9 +11,43 @@ import java.util.concurrent.TimeUnit;
  * @DATE: 2023/5/26 0:22
  */
 public class VolatileVisibilityTest {
-    private static boolean initFlag = false;
 
-    public static void main(String[] args) {
+    static int x = 0, y = 0;
+    static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Set<String> resultSet = new HashSet<>();
+        for (int i = 0; i < 10000000; i++) {
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+            Thread one = new Thread(() -> {
+                a = y;
+                x = 1;
+
+            });
+
+            Thread other = new Thread(() -> {
+                b = x;
+                y = 1;
+            });
+
+            one.start();
+            other.start();
+            one.join();
+            other.join();
+
+            resultSet.add("a=" + a + "." + "b=" + b);
+            System.out.println(resultSet);
+        }
+    }
+
+    private static boolean initFlag = false;
+    // 展示volatile的用途，不加volatile修饰时，第一个线程会进入死循环
+    private static void volatileUsageDemo1() {
         new Thread(() -> {
             System.out.println("waiting data...");
             while (!initFlag) {
@@ -27,7 +63,6 @@ public class VolatileVisibilityTest {
         }
 
         new Thread(VolatileVisibilityTest::prepareData).start();
-
     }
 
     public static void prepareData(){
