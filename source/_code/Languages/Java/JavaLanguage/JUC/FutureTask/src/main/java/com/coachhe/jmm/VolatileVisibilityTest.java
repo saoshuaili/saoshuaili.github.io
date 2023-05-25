ef1a8edc54ff876109cfdf7e88f6ebaf1b6f8dc1
coachhe <624904389@qq.com>
2023-05-26 00:34:36 +0800

    vault backup: 2023-05-26 00:34:36

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/VolatileVisibilityTest.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/VolatileVisibilityTest.java
@@ -1,8 +1,38 @@
-package com.coachhe.jmm;/** 
+package com.coachhe.jmm;
+
+import java.util.concurrent.TimeUnit;
+
+/**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/26 0:22
  */
 public class VolatileVisibilityTest {
+    private static boolean initFlag = false;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            System.out.println("waiting data...");
+            while (!initFlag) {
+
+            }
+            System.out.println("--------------------success");
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(VolatileVisibilityTest::prepareData).start();
+
+    }
+
+    public static void prepareData(){
+        System.out.println("----prepare data...");
+        initFlag = true;
+        System.out.println("----prepare data end...");
+    }
 }
