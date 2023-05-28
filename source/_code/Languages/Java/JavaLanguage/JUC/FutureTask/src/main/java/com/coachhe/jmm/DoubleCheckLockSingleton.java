4d30b281fb82e579c1af321264bd379a78f80305
coachhe <624904389@qq.com>
2023-05-28 18:07:38 +0800

    vault backup: 2023-05-28 18:07:38

--- source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/DoubleCheckLockSingleton.java
+++ source/_code/Languages/Java/JavaLanguage/JUC/FutureTask/src/main/java/com/coachhe/jmm/DoubleCheckLockSingleton.java
@@ -1,8 +1,33 @@
-package com.coachhe.jmm;/** 
+package com.coachhe.jmm;
+
+/**
  * @PROJECT_NAME: JUC
  * @DESCRIPTION:
  * @AUTHOR: CoachHe
  * @DATE: 2023/5/28 18:01
  */
-public class dclTest {
+public class DoubleCheckLockSingleton {
+
+    private static DoubleCheckLockSingleton instance = null;
+
+    private DoubleCheckLockSingleton() {
+
+    }
+
+    // DCL
+    public static DoubleCheckLockSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DoubleCheckLockSingleton.class) {
+                if (instance == null) {
+                    instance = new DoubleCheckLockSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        DoubleCheckLockSingleton instance = DoubleCheckLockSingleton.getInstance();
+    }
+
 }
